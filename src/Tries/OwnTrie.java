6fabf347f6e068da505cdb6d187e22398be1e933
Narendra Agrawal <narendrabtechcse@gmail.com>
2022-01-28 14:05:39 +0530

    Tries and Heaps Learning

--- src/Tries/OwnTrie.java
+++ src/Tries/OwnTrie.java
@@ -1,6 +1,61 @@
 package Tries;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class OwnTrie {
 
+    public class TrieNode {
+        public boolean isWord;
+        public Map<Character,TrieNode> childrenMap = new HashMap<Character,TrieNode>();
+    }
 
+    TrieNode root;
+    public OwnTrie()
+    {
+        root = new TrieNode();
+    }
+
+    public void insertIntoTrie(String word)
+    {
+        TrieNode cur = root;
+        for(int i=0;i<word.length();i++)
+        {
+            char ch = word.charAt(i);
+            if(root.childrenMap.get(ch)==null)
+            {
+                root.childrenMap.put(ch,new TrieNode());
+            }
+            cur = cur.childrenMap.get(ch);
+        }
+        cur.isWord = true;
+    }
+
+    public boolean search(String word)
+    {
+        TrieNode cur = root;
+        for(int i=0;i<word.length();i++)
+        {
+            char ch = word.charAt(i);
+            if(root.childrenMap.get(ch)==null)
+                return false;
+
+            cur = cur.childrenMap.get(ch);
+        }
+        return cur.isWord;
+    }
+
+    public boolean isStartsWith(String word)
+    {
+        TrieNode cur = root;
+        for(int i=0;i<word.length();i++)
+        {
+            char ch = word.charAt(i);
+            if(cur.childrenMap.get(ch)==null)
+                return false;
+
+            cur = cur.childrenMap.get(ch);
+        }
+        return true;
+    }
 }
